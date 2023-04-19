c530a37f9109e7874564c66257b1ad9b08b12875
sebastian-rodriguez-rincon <sr249860@gmail.com>
2023-04-19 10:28:43 -0500

    implementación
    Se añade adaptador para el mostrado de la lista de razas de gatos

--- app/src/main/java/com/example/kotlin_prueba/UI/adapter/CatAdapter.kt
+++ app/src/main/java/com/example/kotlin_prueba/UI/adapter/CatAdapter.kt
@@ -1,35 +1,51 @@
 package com.example.kotlin_prueba.UI.adapter
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.kotlin_prueba.DataFuncionality.CatlModel
-import com.example.kotlin_prueba.R
-import com.example.kotlin_prueba.network.CatApi
+import com.example.kotlin_prueba.databinding.CardCatBinding
 
-class CatAdapter(): RecyclerView.Adapter<CatAdapter.SearchViewHolder>(){
+class CatAdapter() : RecyclerView.Adapter<CatAdapter.SearchViewHolder>() {
 
-    var catList : List<CatlModel> = emptyList<CatlModel>()
+    var catList: List<CatlModel> = emptyList<CatlModel>()
+    private lateinit var context: Context
 
-    fun setData(list: ArrayList<CatlModel>){
+    fun setData(list: ArrayList<CatlModel>) {
         catList = list
         notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
-        return SearchViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.card_cat, parent,false))
+        context = parent.context
+        val binding = CardCatBinding.inflate(LayoutInflater.from(context), parent, false)
+        return SearchViewHolder(binding)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+        val url = "https://cdn2.thecatapi.com/images/"
         val cat = catList[position]
-        println("Nombre: " + cat.name)
+        holder.binding.name.text = "Nombre: " + cat.name
+        holder.binding.intelligence.text = "Inteligencia: " + cat.intelligence
+        holder.binding.origin.text = "Pais de origen: " + cat.origin
+
+        Glide.with(context)
+            .load(url + cat.reference_image_id + ".jpg")
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+            .centerCrop()
+            .into(holder.binding.image)
+
     }
 
     override fun getItemCount(): Int {
-       return catList.size
+        return catList.size
     }
 
-    class SearchViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+    inner class SearchViewHolder(val binding: CardCatBinding) :
+        RecyclerView.ViewHolder(binding.root)
 }
--- app/src/main/java/com/example/kotlin_prueba/UI/view/CatView.kt
+++ app/src/main/java/com/example/kotlin_prueba/UI/view/CatView.kt
@@ -1,7 +1,14 @@
 package com.example.kotlin_prueba.UI.view
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,8 +19,9 @@ import com.example.kotlin_prueba.UI.viewmodel.CatViewModel
 class CatView : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel : CatViewModel
+    private lateinit var viewModel: CatViewModel
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,13 +29,52 @@ class CatView : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this).get(CatViewModel::class.java)
 
-        binding.catListRecyclerView.layoutManager = LinearLayoutManager(this)
-        binding.catListRecyclerView.adapter = CatAdapter()
-        viewModel.getCatList()
-        viewModel.catList.observe(this, Observer { list ->(binding.catListRecyclerView.adapter as CatAdapter).setData(list) })
-        //viewModel.catList.observe()
-        println("Holaaaaaa")
+        apiService()
 
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun apiService() {
+        if(isConnected(this)){
+            binding.catListRecyclerView.visibility = View.VISIBLE
+            binding.title.visibility = View.VISIBLE
+            binding.reintentar.visibility  = View.GONE
+            binding.catListRecyclerView.layoutManager = LinearLayoutManager(this)
+            binding.catListRecyclerView.adapter = CatAdapter()
+            viewModel.getCatList()
+            viewModel.catList.observe(
+                this,
+                Observer { list -> (binding.catListRecyclerView.adapter as CatAdapter).setData(list) })
+
+        }else{
+            binding.reintentar.visibility  = View.VISIBLE
+            binding.reintentar.text = "NO TIENE CONEXIÓN DE INTERNET \n HAGA CLIC PARA REINTENTAR"
+            binding.reintentar.setOnClickListener{
+                apiService()
+            }
+        }
+
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun isConnected(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (connectivityManager != null) {
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                    return true
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                    return true
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                    return true
+                }
+            }
+        }
+        return false
     }
 
 }
